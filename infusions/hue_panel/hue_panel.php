5af9b090da5068596593705d974d2082f7dc335f
xxyy <xxyy598@gmail.com>
2011-01-04 17:46:40 +0100

    Revision 6

--- infusions/hue_panel/hue_panel.php
+++ infusions/hue_panel/hue_panel.php
@@ -1,4 +1,18 @@
 <?php
+/*---------------------------------------------------------------------------+
+| Hausübungsinformationssystem HÜ
+| Copyright (C) 2010 - 2010 
+| http://blacktigers.bplaced.net/
++----------------------------------------------------------------------------+
+| Filename: hue_panel.php
+| Author: xxyy
++----------------------------------------------------------------------------+
+| This program is released as free software under the Affero GPL license.
+| You can redistribute it and/or modify it under the terms of this license
+| which you can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this copyright header is
+| strictly prohibited without written permission from the original author(s).
++---------------------------------------------------------------------------*/
 if(!defined("PIMPED_FUSION")) {
 require_once "../../maincore.php";
 require_once TEMPLATES."header.php";
